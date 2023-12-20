41fcfee54434c08085d1693961a6b6847732d8c0
noahtajstudent <ntajal2829@gmail.com>
2023-12-20 00:18:45 -0500

    2

--- model.py
+++ model.py
@@ -27,28 +27,29 @@ class CustomImageDataset(Dataset):
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
-        self.init_size = 256 // 4  # Initial size before upscaling
-        self.l1 = nn.Sequential(nn.Linear(100, 128 * self.init_size ** 2))
 
-        self.conv_blocks = nn.Sequential(
+        self.model = nn.Sequential(
+            # Assuming input images are RGB, so in_channels = 3
+            nn.Conv2d(3, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+
             nn.Upsample(scale_factor=2),
             nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128, 0.8),
+            nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
+
             nn.Upsample(scale_factor=2),
             nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64, 0.8),
+            nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2, inplace=True),
+
             nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1),
-            nn.Tanh()
+            nn.Tanh()  # Output is an RGB image
         )
 
-    def forward(self, z):
-        out = self.l1(z)
-        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
-        img = self.conv_blocks(out)
-        return img
+    def forward(self, img):
+        return self.model(img)
 
 
 
