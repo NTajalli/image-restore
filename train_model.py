ae3bb12fadc14e3336387f332080ea813cbecc45
noahtajstudent <ntajal2829@gmail.com>
2023-12-20 21:04:08 -0500

    2

--- train_model.py
+++ train_model.py
@@ -9,61 +9,49 @@ from torch import optim
 def train(generator, discriminator, dataloader, optimizer_G, optimizer_D, criterion, L1_loss, L1_lambda, epochs, device):
     generator.to(device)
     discriminator.to(device)
-    
+
     for epoch in range(epochs):
         for i, data in enumerate(dataloader):
-            L = data['L'].to(device)  # L channel
-            ab = data['ab'].to(device)  # ab channels
-            vintage = data['vintage'].to(device)  # Vintage images
+            L = data['L'].to(device)  # Shape: [batch_size, height, width]
+            ab = data['ab'].to(device)  # Shape: [batch_size, 2, height, width]
+            vintage = data['vintage'].to(device)
+
+            # Ensure L has a single channel and is a 4D tensor
+            L = L.unsqueeze(1)  # Shape: [batch_size, 1, height, width]
 
             # Adversarial ground truths
             valid = torch.ones((L.size(0), 1), device=device, requires_grad=False)
             fake = torch.zeros((L.size(0), 1), device=device, requires_grad=False)
 
-            # -----------------
-            #  Train Generator
-            # -----------------
+            # Train Generator
             optimizer_G.zero_grad()
+            gen_ab = generator(vintage)  # Shape: [batch_size, 2, height, width]
 
-            # Generate a batch of ab channels from the vintage images
-            gen_ab = generator(vintage)
-
-            # Concatenate L channel with fake ab channels for discriminator input
+            # Concatenate L channel with fake ab channels
             fake_images_lab = torch.cat((L, gen_ab), 1)
 
-            # Adversarial loss to fool the discriminator
+            # Adversarial and L1 loss
             g_loss_adv = criterion(discriminator(fake_images_lab), valid)
-            # L1 loss for colorization accuracy
             g_loss_L1 = L1_loss(gen_ab, ab) * L1_lambda
             g_loss = g_loss_adv + g_loss_L1
-
             g_loss.backward()
             optimizer_G.step()
 
-            # ---------------------
-            #  Train Discriminator
-            # ---------------------
+            # Train Discriminator
             optimizer_D.zero_grad()
-
-            # Real images
             real_images_lab = torch.cat((L, ab), 1)
-
-            # Real loss
             real_loss = criterion(discriminator(real_images_lab), valid)
-            # Fake loss
             fake_loss = criterion(discriminator(fake_images_lab.detach()), fake)
-
             d_loss = (real_loss + fake_loss) / 2
-
             d_loss.backward()
             optimizer_D.step()
 
-            # Print training status
+            # Logging
             print(f"[Epoch {epoch}/{epochs}] [Batch {i}/{len(dataloader)}] [D loss: {d_loss.item()}] [G loss: {g_loss.item()}]")
 
+            # Save Images
             batches_done = epoch * len(dataloader) + i
             if batches_done % 10 == 0:
-                # Save sample images
                 sample_images = torch.cat((vintage.data, gen_ab.data, ab.data), -1)
                 save_image(sample_images, f"images/{batches_done}.png", nrow=5, normalize=True)
 
