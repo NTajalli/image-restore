fcb9a5a346df90f0de62eb6a4f72631942e9fb14
noahtajstudent <ntajal2829@gmail.com>
2023-12-21 16:17:02 -0500

    17

--- model.py
+++ model.py
@@ -44,11 +44,13 @@ class CustomImageDataset(Dataset):
                 vintage_image = self.transform(vintage_image)
                 color_image = self.transform(color_image)
 
-        # Convert color image to Lab color space using skimage
+        # Convert color image to Lab color space
         color_image_np = np.array(color_image)
-        color_image_lab = rgb2lab(color_image_np).astype("float32")
         
-        # Normalize Lab channels
+        # Permute dimensions from (C, H, W) to (H, W, C) for skimage compatibility
+        color_image_np = np.transpose(color_image_np, (1, 2, 0))
+
+        color_image_lab = rgb2lab(color_image_np).astype("float32")  # Convert RGB to Lab
         L = color_image_lab[:, :, 0] / 50. - 1.  # Normalize L channel to [-1, 1]
         ab = color_image_lab[:, :, 1:] / 110.  # Normalize ab channels to [-1, 1]
 
