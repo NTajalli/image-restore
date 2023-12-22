e4b75dafc614262c8b3daba540a8d81e7823be9e
noahtajstudent <ntajal2829@gmail.com>
2023-12-21 22:34:34 -0500

    DONE

--- training_script.py
+++ training_script.py
@@ -4,6 +4,7 @@ from dataset import *
 from models import MainModel
 from utils import *
 from tqdm import tqdm
+import os
 
 def train_val_split(vintage_dir, val_split=0.2):
     filenames = [f for f in os.listdir(vintage_dir) if os.path.isfile(os.path.join(vintage_dir, f))]
@@ -14,8 +15,9 @@ def train_val_split(vintage_dir, val_split=0.2):
     return train_filenames, val_filenames
 
 
-def train_model(model, train_dl, val_dl, epochs, display_every=200):
+def train_model(model, train_dl, val_dl, epochs, display_every=200, snapshot_path='./model_snapshots'):
     data = next(iter(val_dl)) # getting a batch for visualizing the model output after fixed intrvals
+    os.makedirs(snapshot_path, exist_ok=True)
     for e in range(epochs):
         loss_meter_dict = create_loss_meters() # function returing a dictionary of objects to 
         i = 0                                  # log the losses of the complete network
@@ -29,6 +31,12 @@ def train_model(model, train_dl, val_dl, epochs, display_every=200):
                 print(f"Iteration {i}/{len(train_dl)}")
                 log_results(loss_meter_dict) # function to print out the losses
                 visualize(model, data, save=False) # function displaying the model's outputs
+                
+                # Save model snapshots
+                snapshot_filename = f"model_epoch_{e+1}_iter_{i}.pth"
+                snapshot_filepath = os.path.join(snapshot_path, snapshot_filename)
+                torch.save(model.state_dict(), snapshot_filepath)
+                print(f"Saved model snapshot to {snapshot_filepath}")
 
 vintage_dir = './vintage_images'
 color_dir = './downloaded_images'
@@ -52,5 +60,9 @@ train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=False)
 
 model = MainModel()
+
+if torch.cuda.device_count() > 1:
+    print(f"Using {torch.cuda.device_count()} GPUs!")
+    model = torch.nn.DataParallel(model)
 
 train_model(model, train_dataloader, val_dataloader, 100)
